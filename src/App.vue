410eb84d70c7fe6140a71617336ae6575f3a4705
tvdr <tibor.barta@erba.hu>
2017-05-13 22:27:09 +0200

    weather app

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="app">
+        <div class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-responsive-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/#/">TWC Weather</a>
+                </div>
+                <div class="navbar-collapse collapse navbar-responsive-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/#/settings">Settings</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <router-view :usersettings="usersettings"></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data()
+            {
+                return {
+                    usersettings: {
+                        temp: {
+                            min: 20,
+                            max: 30
+                        },
+
+                        humidity: {
+                            min: 20,
+                            max: 30
+                        },
+                        city: '',
+                        useCoordsIfAvailable: true
+                    }
+                }
+            },
+            mounted(){
+                $.material.init()
+            }
+}
+
+</script>
---
+++ src/components/Settings.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="container">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <h1>Your personal definition of good weather</h1>
+            </div>
+        </div>
+        <div class="row">
+            <form id="usersettings">
+                <div class="col-xs-12 col-sm-6 form-group">
+                    <label for="tempmin">Temperature MIN: {{usersettings.temp.min}} °C </label>
+                    <input class="form-control" id="tempmin" type="range" v-model.number="usersettings.temp.min">
+                </div>
+                <div class="col-xs-12 col-sm-6 form-group">
+                    <label for="tempmax">Temperature MAX: {{usersettings.temp.max}} °C </label>
+                    <input class="form-control" id="tempmax" type="range" v-model.number="usersettings.temp.max">
+                </div>
+
+                <div class="col-xs-12 col-sm-6 form-group">
+                    <label for="humiditymin">Humidity MIN: {{usersettings.humidity.min}} %</label>
+                    <input class="form-control" id="humiditymin" type="range" v-model.number="usersettings.humidity.min">
+                </div>
+                <div class="col-xs-12 col-sm-6 form-group">
+                    <label for="humiditymax">Humidity MAX: {{usersettings.humidity.max}} %</label>
+                    <input class="form-control" id="humiditymax" type="range" v-model.number="usersettings.humidity.max">
+                </div>
+
+                <div class="col-xs-12 col-sm-6 form-group label-floating">
+                    <label class="control-label" for="city">City</label>
+                    <input class="form-control" id="city" type="text" v-model="usersettings.city" :disabled="usersettings.useCoordsIfAvailable ? true : false">
+                </div>
+
+                <div class="col-xs-12 col-sm-6 form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" v-model="usersettings.useCoordsIfAvailable"> Use geolocation if available
+                        </label>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'settings',
+        props:['usersettings'],
+
+        mounted(){
+                $.material.init()
+
+            }
+
+
+    }
+
+
+</script>
